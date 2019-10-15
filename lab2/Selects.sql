5fadc9c637396a412c5ddf094c4f8522e35253e8
U-DESKTOP-IH4NTDI\Artur <Artur Yumaev>
2019-10-15 19:27:16 +0300

    lab2

---
+++ lab2/Selects.sql
@@ -0,0 +1,25 @@
+﻿/*
+	1. Узнать 3 города с самым большим числом работников
+*/
+/*
+select top 3 city, count(*) as cnt from (
+	select c.city, e.id
+	from employee e
+	inner join companies as c
+	on e.company = c.company
+) as n_tbl
+group by city
+order by cnt desc
+
+go
+*/
+
+/*
+	2. Найти имена, фамилии и телефон рабочих,
+	родившихся после 80го года
+*/
+select e.first_name, e.last_name, e.phone_number, p.issue_date
+from employee e
+inner join passports as p on e.PASSPORT_FK = p.id
+where issue_date > '1980-01-01'
+order by issue_date
