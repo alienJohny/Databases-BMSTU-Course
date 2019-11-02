41d0b285f9553fcfd3b428265c49ee296f9cdc44
U-DESKTOP-IH4NTDI\Artur <Artur Yumaev>
2019-11-02 15:48:27 +0300

    update

---
+++ lab3/Tasks B/func3.sql
@@ -0,0 +1,27 @@
+﻿use data
+
+go
+
+create procedure testCursor
+as
+begin
+    -- Объявляем переменную
+    declare @TableName varchar(255)
+    -- Объявляем курсор
+    declare TableCursor cursor for
+    select TABLE_NAME from INFORMATION_SCHEMA.TABLES
+    where TABLE_TYPE = 'BASE TABLE'
+    -- Открываем курсор и выполняем извлечение первой записи 
+    open TableCursor
+    fetch next from TableCursor into @TableName
+    -- Проходим в цикле все записи из множества
+    while @@FETCH_STATUS = 0
+    begin
+        print @TableName
+    fetch next from TableCursor into @TableName
+    end
+    -- Убираем за собой «хвосты»
+    close TableCursor
+    deallocate TableCursor
+end
+;
